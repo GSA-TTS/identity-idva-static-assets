3b0034dce82af19b658ee05018c4759b3d94eb9f
Peter Shumate <shumatepfs@gmail.com>
2022-05-03 14:54:16 -0400

    Host JavaScript for IDVA Screen Translations (#6)
    * Add language translation scrip and deployment configuration
    
    * Replace env variable in lang script
    
    * Add white space
    
    * Remove uneeded whitespace
    
    * Specify env variable only for replacement

---
+++ template/lang-template.js
@@ -0,0 +1,57 @@
+const lang = {
+    EN: "en",
+    ES: "es",
+    FR: "fr"
+}
+const defaultLocale = lang.EN;
+
+let locale;
+var translations = {};
+
+$("#i18n-mobile-toggle").click(function () { $("#i18n-mobile-dropdown").toggleClass("no-display") })
+$("#i18n-desktop-toggle").click(function () { $("#i18n-desktop-dropdown").toggleClass("no-display") })
+
+$(".i18n-en").click(function () { setLocale(lang.EN) })
+$(".i18n-es").click(function () { setLocale(lang.ES) })
+$(".i18n-fr").click(function () { setLocale(lang.FR) })
+
+// When the page content is ready...
+document.addEventListener("DOMContentLoaded", () => {
+    setLocale(defaultLocale);
+});
+
+async function setLocale(newLocale) {
+    if (newLocale === locale) return;
+    const newTranslations =
+        await fetchTranslationsFor(newLocale);
+    locale = newLocale;
+    translations = newTranslations;
+    translatePage();
+}
+
+// Get translation json object
+async function fetchTranslationsFor(newLocale) {
+    return $.getJSON(`https://identity-idva-static-assets-${ENVIRONMENT_NAME}.app.cloud.gov/js/locale/languages/${newLocale}.json`, function (data) {
+        console.log(data);
+        return data;
+    });
+}
+
+function translatePage() {
+    document
+        .querySelectorAll("[data-i18n-key]")
+        .forEach(translateElement);
+}
+
+function translateElement(element) {
+    const key = element.getAttribute("data-i18n-key");
+    const translation = getElement(key);
+    element.innerHTML = translation;
+}
+
+// use Array Reduce to recursively fetch dynamic json keys
+function getElement(nest) {
+    return nest.split('.').reduce(function (o, k) {
+        return o && o[k];
+    }, translations[locale])
+}
