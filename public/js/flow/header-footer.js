0acf954e0dc412ba4fd92ff13877e2b4674e34c7
shumatepf <shumatepfs@gmail.com>
2023-02-21 11:32:36 -0500

    Add single script for replacing header and footer

---
+++ public/js/flow/header-footer.js
@@ -0,0 +1,4 @@
+function initHeaderFooter() {
+    $("header").load("https://identity-idva-static-assets-dev.app.cloud.gov/html/header.html");
+    $("footer").load("https://identity-idva-static-assets-dev.app.cloud.gov/html/footer.html");
+}
